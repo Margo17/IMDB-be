93868c8e03314ccdfeb6dbbaefada7cfcfb0d981
Martynas Goberis <martynasgoberis@gmail.com>
2024-05-12 18:01:18 +0300

    Add DI abstractions nuget and ApplicationServiceCollectionExtensions to register Application services in API layer

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,4 +1,6 @@
-var builder = WebApplication.CreateBuilder(args);
+using Movies.Application;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,8 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
