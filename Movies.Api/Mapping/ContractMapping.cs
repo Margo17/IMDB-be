746460e75cbc08c57a327e4fdf5f041d6f582b50
Martynas Goberis <martynasgoberis@gmail.com>
2024-05-13 18:29:54 +0300

    Add Movie Create resource and Mapping extension dir

---
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,18 @@
+using Movies.Application.Models;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Mapping;
+
+public static class ContractMapping
+{
+    public static Movie MapToMovie(this CreateMovieRequest request)
+    {
+        return new Movie
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+        };
+    }
+}
