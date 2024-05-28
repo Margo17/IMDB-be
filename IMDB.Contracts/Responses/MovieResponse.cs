a50b44159158e007da9ece20f5b767a3580833f9
Martynas Goberis <martynasgoberis@gmail.com>
2024-05-28 15:11:12 +0300

    Add ratings to MovieResponse contract

--- IMDB.Contracts/Responses/MovieResponse.cs
+++ IMDB.Contracts/Responses/MovieResponse.cs
@@ -8,6 +8,10 @@ public class MovieResponse
 
     public required string Slug { get; init; }
 
+    public float? Rating { get; init; }
+    
+    public int? UserRating { get; init; }
+
     public required int YearOfRelease { get; init; }
 
     public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
