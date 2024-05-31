969a7071898d55b70cad8e99554b36d3efcb73cb
Martynas Goberis <martynasgoberis@gmail.com>
2024-05-31 17:01:58 +0300

    Add ability to rate a movie

--- IMDB.Application/Repositories/IRatingRepository.cs
+++ IMDB.Application/Repositories/IRatingRepository.cs
@@ -2,6 +2,8 @@ namespace IMDB.Application.Repositories;
 
 public interface IRatingRepository
 {
+    Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token = default);
+    
     Task<float?> GetRatingAsync(Guid movieId, CancellationToken token = default);
     
     Task<(float? Rating, int? UserRating)> GetRatingAsync(Guid movieId, Guid userId, CancellationToken token = default);
--- IMDB.Application/Repositories/RatingRepository.cs
+++ IMDB.Application/Repositories/RatingRepository.cs
@@ -6,6 +6,20 @@ namespace IMDB.Application.Repositories;
 
 public class RatingRepository(IDbConnectionFactory _dbConnectionFactory) : IRatingRepository
 {
+    public async Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token = default)
+    {
+        using IDbConnection connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+
+        int result = await connection.ExecuteAsync(new CommandDefinition("""
+            insert into ratings(userid, movieid, rating)
+            values (@userId, @movieId, @rating)
+            on conflict (userid, movieid) do update
+            set rating = @rating
+            """, new { userId, movieId, rating }));
+
+        return result > 0;
+    }
+
     public async Task<float?> GetRatingAsync(Guid movieId, CancellationToken token = default)
     {
         using IDbConnection connection = await _dbConnectionFactory.CreateConnectionAsync(token);
